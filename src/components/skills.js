fdd3f91214ea349f81b4f3677470e2bd905bb7f7
salah <slah0982@gmail.com>
2024-06-10 00:08:01 +0200

    adding more skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,8 +1,10 @@
 import './skills.css'
 import MainHeader from './main-header';
 import SkillsProgress from './skill-prograss';
-import { DiJavascript } from 'react-icons/di';
-import { FaCss3, FaHtml5, FaReact } from 'react-icons/fa6';
+import { DiJavascript, DiMysql } from 'react-icons/di';
+import { FaBootstrap, FaCss3, FaHtml5, FaReact } from 'react-icons/fa6';
+import { BiLogoTailwindCss } from 'react-icons/bi';
+import { SiExpress } from 'react-icons/si';
 
 
 
@@ -35,9 +37,37 @@ export default function Skills(){
 
                 {/* react */}
                 <div className="doughnut">
-                    <SkillsProgress percentage={98}> 98 %</SkillsProgress>
+                    <SkillsProgress percentage={90}> 90 %</SkillsProgress>
 
                     <h2><FaReact style={{color: 'blue'}}/> React.js</h2>     
+                </div>
+
+                {/* Bootstrap */}
+                <div className="doughnut">
+                    <SkillsProgress percentage={88}> 88 %</SkillsProgress>
+
+                    <h2><FaBootstrap style={{color: 'purple'}}/> Bootstrap</h2>     
+                </div>
+
+                {/* Tailwind */}
+                <div className="doughnut">
+                    <SkillsProgress percentage={87}> 87 %</SkillsProgress>
+
+                    <h2><BiLogoTailwindCss style={{color: 'lightblue'}}/> Tailwind</h2>     
+                </div>
+
+                {/* Express */}
+                <div className="doughnut">
+                    <SkillsProgress percentage={95}> 95 %</SkillsProgress>
+
+                    <h2><SiExpress style={{color: 'green'}}/> Express</h2>     
+                </div>
+
+                {/* Mysql */}
+                <div className="doughnut">
+                    <SkillsProgress percentage={93}> 93 %</SkillsProgress>
+
+                    <h2><DiMysql style={{color: 'blue'}}/> Mysql</h2>
                 </div>
             </div>
 
